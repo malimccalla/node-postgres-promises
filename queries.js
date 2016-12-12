2e18d2d8217dea71e68c32f861dfff1d6fc7f39e
Mali Michael <malimccalla@gmail.com>
2016-12-12 08:38:07 +0000

    Get all query

--- queries.js
+++ queries.js
@@ -3,11 +3,27 @@ const promise = require('bluebird');
 const options = { promiseLib: promise };
 const pgp = require('pg-promise')(options);
 const connectionString = 'postgres://localhost:5432/puppies';
+const db = pgp(connectionString);
+
+const getAllPuppies = (req, res, next) => {
+  db.any('SELECT * FROM pups')
+    .then((data) => {
+      res.status(200)
+        .json({
+          status: 200,
+          data,
+          message: 'Retrieved all puppies'
+        });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
 
 module.exports = {
-  getAllPuppies: getAllPuppies,
-  getSinglePuppy: getSinglePuppy,
-  createPuppy: createPuppy,
-  updatePuppy: updatePuppy,
-  removePuppy: removePuppy
+  getAllPuppies: getAllPuppies
+  // getSinglePuppy: getSinglePuppy,
+  // createPuppy: createPuppy,
+  // updatePuppy: updatePuppy,
+  // removePuppy: removePuppy
 };
