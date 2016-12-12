f1476843012557924b528439cc12410ba7476969
Mali Michael <malimccalla@gmail.com>
2016-12-12 11:40:16 +0000

    Update and create queries

--- queries.js
+++ queries.js
@@ -36,10 +36,47 @@ const getSinglePuppy = (req, res, next) => {
     });
 }
 
+const createPuppy = (req, res, next) => {
+  req.body.age = parseInt(req.body.age); // only part of body that isn't a string
+  console.log(req.body);
+  db.none('INSERT INTO pups(name, breed, age, sex) ' +
+          'VALUES (${name}, ${breed}, $<age>, $[sex])', req.body)
+    .then(() => {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one puppy'
+        });
+    })
+    .catch((err) => {
+      return next(err);
+    })
+}
+
+const updatePuppy = (req, res, next) => {
+  console.log(req.body);
+  const { name, breed, sex, age } = req.body;
+  const id = parseInt(req.params.id);
+  console.log(id);
+  console.log(name);
+  db.none('UPDATE pups SET name=$1, breed=$2, age=$3, sex=$4 WHERE id=$5',
+          [name, breed, age, sex, id])
+    .then(() => {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated puppy'
+        });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+}
+
 module.exports = {
   getAllPuppies: getAllPuppies,
   getSinglePuppy: getSinglePuppy,
-  // createPuppy: createPuppy,
-  // updatePuppy: updatePuppy,
+  createPuppy: createPuppy,
+  updatePuppy: updatePuppy,
   // removePuppy: removePuppy
 };
